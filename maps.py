c42d0c22d1ebcd3797d2ec7f8b7d9fd4ffb982d9
Anthony Kiplimo <anthonylimo90@gmail.com>
2018-08-22 16:40:07 +0300

    Initial Commit
    Initialized Folio and Pandas to create maps from existsing data

---
+++ maps.py
@@ -0,0 +1,19 @@
+import folium
+import pandas
+
+data = pandas.read_csv("Volcanoes.txt")
+lat = list(data["LAT"])
+lon = list(data["LON"])
+
+map = folium.Map(location=[25.58,-92.09], zoom_start=8, tiles="Mapbox Bright")
+
+fg = folium.FeatureGroup(name="My Map")
+
+list_of_coordinates = [[25.7, 93.2],[26.9,89.0]]
+
+for lt, lg in zip(lat, lon):
+    fg.add_child(folium.Marker(location=[lt,lg],popup="Locations", icon=folium.Icon(color="green")))
+
+map.add_child(fg)
+
+map.save("Test.html")
