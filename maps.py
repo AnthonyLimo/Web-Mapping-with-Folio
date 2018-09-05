d043887c8d2cbd3acaedfbad886641a7cdaea8a1
Anthony Kiplimo <anthonylimo90@gmail.com>
2018-09-05 17:14:41 +0300

    Nothing much changed

--- maps.py
+++ maps.py
@@ -4,13 +4,14 @@ import pandas
 data = pandas.read_csv("Volcanoes.txt")
 lat = list(data["LAT"])
 lon = list(data["LON"])
+elev = list(data["ELEV"])
 
 map = folium.Map(location=[25.58,-92.09], zoom_start=8, tiles="Mapbox Bright")
 
 fg = folium.FeatureGroup(name="My Map")
 
-for lt, lg in zip(lat, lon):
-    fg.add_child(folium.Marker(location=[lt,lg],popup="Locations", icon=folium.Icon(color="green")))
+for lt, lg, el in zip(lat, lon, elev):
+    fg.add_child(folium.Marker(location=[lt,lg], popup=str(el), icon=folium.Icon(color="green")))
 
 map.add_child(fg)
 
